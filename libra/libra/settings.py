3fe66abed41110d530b8e0ea4e69922496f44984
БаТыРхАн <batrkhan4.0@gmail.com>
2026-04-24 11:48:14 +0500

    feat: add error logging to console

--- libra/libra/settings.py
+++ libra/libra/settings.py
@@ -224,3 +224,24 @@ CLOUDINARY_STORAGE = {
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'root': {
+        'handlers': ['console'],
+        'level': 'WARNING',
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+            'level': 'ERROR',
+            'propagate': False,
+        },
+    },
+}
